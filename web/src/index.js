4e35b73a1a157b5055a174ec6e2deb3f1d29810a
Lopukhovych <volodymyr.lopukhovychh@gmail.com>
2019-03-21 13:44:32 +0200

    refactored web/src/index.js

--- web/src/index.js
+++ web/src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {BrowserRouter} from 'react-router-dom';
-// import {Provider} from 'react-redux';
-// import {createHistory} from 'history/createBrowserHistory';
-
-
 
 import * as serviceWorker from './serviceWorker';
 import store from './reducers/index';
 
 import './index.css';
-// import App from './App';
 import Root from './containers/Root';
-
-// const history = createHistory();
-// const root = (
-//     <Provider store={store}>
-//         <BrowserRouter>
-//             <App/>
-//         </BrowserRouter>
-//     </Provider>
-// );
 
 const root = (<Root store={store} />);
 
